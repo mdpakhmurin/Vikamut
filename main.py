1b300ac751272a6902a47cfd7af78dc9606d7116
Michael Pakhmurin <pmdmix1@gmail.com>
2025-11-25 19:31:58 +0700

    Больше устойчивости

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
+import time
+
 import config
 import filters
 
@@ -27,32 +29,45 @@ def reload_filters_if_needed():
             last_modified = current_mtime
             print(datetime.now(), "\t", "filters.py reloaded")
     except Exception as e:
-        print(datetime.now(), "\t", "error loading filters.py")
+        print(datetime.now(), "\t", "error loading filters.py", e)
 
 
-for event in longpoll.listen():
+while True:
     try:
-        reload_filters_if_needed()
-    except:
-        print(datetime.now(), "\t", "error loading filters.py")
+        for event in longpoll.listen():
+            try:
+                reload_filters_if_needed()
+            except:
+                print(datetime.now(), "\t", "error loading filters.py")
 
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        msg = event.message
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                msg = event.message
 
-        try:
-            is_conversation = msg.conversation_message_id
-            if (not is_conversation):
-                continue
+                try:
+                    is_conversation = msg.conversation_message_id
+                    if not is_conversation:
+                        continue
 
-            if (config.peer_ids is not None and len(config.peer_ids) != 0 and msg.peer_id not in config.peer_ids):
-                continue
-            
-            if filters.is_delete(msg):
-                vk.messages.delete(
-                    peer_id=msg.peer_id,
-                    cmids=[msg.conversation_message_id],
-                    delete_for_all=1
-                )
-                print(datetime.now(), "\t", f"Message deleted (from: https://vk.com/id{msg.from_id}, peer_id:{msg.peer_id}): {msg.text}")
-        except Exception as e:
-            print(datetime.now(), "\t", "error filtering message")
+                    if (config.peer_ids 
+                        and len(config.peer_ids) != 0 
+                        and msg.peer_id not in config.peer_ids):
+                        continue
+
+                    if filters.is_delete(msg):
+                        vk.messages.delete(
+                            peer_id=msg.peer_id,
+                            cmids=[msg.conversation_message_id],
+                            delete_for_all=1
+                        )
+                        print(
+                            datetime.now(), 
+                            "\t",
+                            f"Message deleted (from: https://vk.com/id{msg.from_id}, peer_id:{msg.peer_id}): {msg.text}"
+                        )
+                except Exception as e:
+                    print(datetime.now(), "\t", "error filtering message:", e)
+
+    except Exception as e:
+        print(datetime.now(), "\t", "Unexpected error:", e)
+        time.sleep(30)
+        continue
